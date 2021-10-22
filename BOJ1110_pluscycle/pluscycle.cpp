45f5cbb4e61107dd84eb73db9f68d0abf1b336ba
wjoh0315 <65387631+wjoh0315@users.noreply.github.com>
2021-10-22 21:43:50 +0900

    BOJ1110_4344_Solved1

---
+++ BOJ1110_pluscycle/pluscycle.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int n;
+    cin >> n;
+
+    int tmp = n;
+    int count = 0;
+    while (true)
+    {
+        tmp = tmp % 10 * 10 + (tmp / 10 + tmp % 10) % 10;
+        count++;
+        if (tmp == n)
+            break;
+    }
+    cout << count << '\n';
+}
---
+++ BOJ4344_higherthanavr/higherthanavr.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+
+    int c;
+    cin >> c;
+
+    cout << fixed;
+    cout.precision(3);
+
+    int n;
+    int cur;
+    int cnt1 = 0;
+    int cnt2 = 0;
+    double avr = 0;
+    for (int i=0; i < c; i++)
+    {
+        cin >> n;
+        cnt1 = 0;
+        cnt2 = 0;
+        
+        int *score = new int[n];
+        for (int j=0; j < n; j++)
+        {
+            cin >> cur;
+            cnt1 += cur;
+            score[j] = cur;
+        }
+        avr = (double)cnt1 / n;
+
+        for (int j=0; j < n; j++)
+            cnt2 += score[j] > avr ? 1 : 0;
+
+        cout << (double)cnt2 / n * 100 << '%' << '\n';
+    }
+}
